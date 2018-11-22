4eee686ef27f806bd91b7dc728f70dbdadc96bbe
Vincent Demeester <vincent@sbr.pm>
2018-11-22 19:28:07 +0100

    go-containerregistry: bump to november 22th version
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/go-containerregistry/default.nix
+++ pkgs/go-containerregistry/default.nix
@@ -2,15 +2,15 @@
 
 buildGoPackage rec {
   name = "go-containerregistry-unstable-${version}";
-  version = "2018-09-19";
-  rev = "52f3c54ec23c758ce5375754a5de62f7cd5ebbbc";
+  version = "2018-11-22";
+  rev = "61e4aeff7593142b9b77936a476c38976a67e0db";
 
   goPackagePath = "github.com/google/go-containerregistry";
 
   src = fetchgit {
     inherit rev;
     url = "https://github.com/google/go-containerregistry";
-    sha256 = "1aqx3kcwgga2x7sbwyb4026f5phvnaavla688sjv6bswdxb1nnwr";
+    sha256 = "1l9r4mbpdr23ncfhcpi1f44xrcp5s46j8nhsw9aanhm27mb1nag6";
   };
 
   goDeps = ./deps.nix;
