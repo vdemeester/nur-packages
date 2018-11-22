d3a9dc0409f34b5faba4bb30e829db8597530ebd
Vincent Demeester <vincent@sbr.pm>
2018-11-22 18:44:14 +0100

    prm: 2.1.1 -> 2.2.0
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/prm/default.nix
+++ pkgs/prm/default.nix
@@ -1,9 +1,9 @@
 { stdenv, lib, buildGoPackage, fetchFromGitHub }:
 
 buildGoPackage rec {
-  name = "prm-unstable-${version}";
-  version = "2.1.1";
-  rev = "83ff93d6d9d043d8c3dde54d47e8a82524bb8cf3"; # v2.1.1
+  name = "prm-${version}";
+  version = "2.2.0";
+  rev = "v${version}"; # v2.1.1
 
   goPackagePath = "github.com/ldez/prm";
 
@@ -17,12 +17,11 @@ buildGoPackage rec {
     inherit rev;
     owner = "ldez";
     repo = "prm";
-    sha256 = "0adz9vli1x5f5v2vyfag7m2a9llj7bxih5g3ccjpiz22xk26rc6l";
+    sha256 = "0ywnrzdr7k16xnz3vzl4bxnqsywkyhl9pq0yfkd7i728d7fa989d";
   };
 
   goDeps = ./deps.nix;
 
-  # TODO: add metadata https://nixos.org/nixpkgs/manual/#sec-standard-meta-attributes
   meta = {
     description = "Pull Request Manager for Maintainers";
     homepage = "https://github.com/ldez/prm";
