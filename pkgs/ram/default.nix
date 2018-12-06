dae2cb381ec7f417164f77c86b5e0d70958a36a0
Vincent Demeester <vincent@sbr.pm>
2018-12-06 15:18:39 +0100

    ram: 0.1.0 -> 0.1.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/ram/default.nix
+++ pkgs/ram/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   name = "ram-${version}";
-  version = "0.1.0";
+  version = "0.1.1";
   rev = "v${version}";
 
   goPackagePath = "github.com/vdemeester/ram";
@@ -11,7 +11,7 @@ buildGoPackage rec {
     inherit rev;
     owner = "vdemeester";
     repo = "ram";
-    sha256 = "1jd3547wfk6sfb6h0asvmil90g10a16qznx8552rzywbr8c6i547";
+    sha256 = "0hxnn20h180pp6qgk0ff57ldiajxf7inwx1m4p5zvbzba6jbyw2s";
   };
 
   meta = {
