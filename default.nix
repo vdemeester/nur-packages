3842c3aaf1784ee30335806eb8739230bc59e6e9
Vincent Demeester <vincent@sbr.pm>
2018-12-17 15:43:57 +0100

    Group upstream package (overriden) vs mine
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- default.nix
+++ default.nix
@@ -4,10 +4,6 @@ rec {
   ape = pkgs.callPackage ./pkgs/ape {};
   athens = pkgs.callPackage ./pkgs/athens {};
   buildkit = pkgs.callPackage ./pkgs/buildkit {};
-  cni = pkgs.callPackage ./pkgs/cni {};
-  cni-plugins = pkgs.callPackage ./pkgs/cni/plugins.nix {};
-  conmon = pkgs.callPackage ./pkgs/podman/conmon.nix {};
-  containerd = pkgs.callPackage ./pkgs/containerd {};
   dobi = pkgs.callPackage ./pkgs/dobi {};
   dep-collector = pkgs.callPackage ./pkgs/dep-collector {};
   envbox = pkgs.callPackage ./pkgs/envbox {};
@@ -15,12 +11,18 @@ rec {
   gogo-protobuf = pkgs.callPackage ./pkgs/gogo-protobuf {};
   knctl = pkgs.callPackage ./pkgs/knctl {};
   krew = pkgs.callPackage ./pkgs/krew {};
-  podman = pkgs.callPackage ./pkgs/podman {};
   prm = pkgs.callPackage ./pkgs/prm {};
   protobuild = pkgs.callPackage ./pkgs/protobuild {};
   ram = pkgs.callPackage ./pkgs/ram {};
-  runc = pkgs.callPackage ./pkgs/runc {};
   s2i = pkgs.callPackage ./pkgs/s2i {};
   slirp4netns = pkgs.callPackage ./pkgs/slirp4netns {};
   stellar = pkgs.callPackage ./pkgs/stellar {};
+  
+  # Upstream
+  cni = pkgs.callPackage ./pkgs/cni {};
+  cni-plugins = pkgs.callPackage ./pkgs/cni/plugins.nix {};
+  conmon = pkgs.callPackage ./pkgs/podman/conmon.nix {};
+  containerd = pkgs.callPackage ./pkgs/containerd {};
+  podman = pkgs.callPackage ./pkgs/podman {};
+  runc = pkgs.callPackage ./pkgs/runc {};
 }
