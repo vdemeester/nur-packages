f648c07c4e71b65edc718060f817cf0921adba9d
Vincent Demeester <vincent@sbr.pm>
2019-02-28 19:18:42 +0100

    Revert "linuxkit: update from upstream PR"
    This reverts commit 539f7f674a24d726a611e86441cf2e49563036b6.

--- pkgs/linuxkit/default.nix
+++ pkgs/linuxkit/default.nix
@@ -1,8 +1,8 @@
 { lib, buildGoPackage, fetchFromGitHub }:
 
 buildGoPackage rec {
-  pname = "linuxkit";
-  version = "0.6";
+  name = "linuxkit-${version}";
+  version = "v0.6";
   rev = "10f07ca1624102de3c2d93da7792be5528c32022";
 
   goPackagePath = "github.com/linuxkit/linuxkit";
@@ -10,16 +10,14 @@ buildGoPackage rec {
   src = fetchFromGitHub {
     owner = "linuxkit";
     repo = "linuxkit";
-    rev = "v${version}";
+    inherit rev;
     sha256 = "12nph1sxgp7l2sb3ar7x8a2rrk2bqphca6snwbcqaqln2ixsh78i";
   };
 
   subPackages = [ "src/cmd/linuxkit" ];
 
-  buildFlagsArray = let t = "${goPackagePath}/src/cmd/linuxkit/version"; in ''
-    -ldflags=
-      -s -X ${t}.GitCommit=${builtins.substring 0 7 rev}
-         -X ${t}.Version=${version}
+  preBuild = ''
+    buildFlagsArray+=("-ldflags" "-X main.GitCommit=${builtins.substring 0 7 rev} -X main.Version=0.6.0")
   '';
 
   meta = {
@@ -27,6 +25,5 @@ buildGoPackage rec {
     license = lib.licenses.asl20;
     homepage = https://github.com/linuxkit/linuxkit;
     platforms = lib.platforms.unix;
-    maintainers = with lib.maintainers; [ vdemeester ];
   };
 }
