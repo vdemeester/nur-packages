7c4b41651871d2c9631082f9c2221c21da7ef64a
Vincent Demeester <vincent@sbr.pm>
2019-06-21 17:10:39 +0200

    tkn: init at 0.1.2
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ pkgs/tkn/default.nix
@@ -0,0 +1,23 @@
+{ stdenv, lib, buildGoPackage, fetchFromGitHub  }:
+
+buildGoPackage rec {
+  pname = "tkn";
+  name = "${pname}-${version}";
+  version = "0.1.2";
+
+  goPackagePath = "github.com/tektoncd/cli";
+
+  src = fetchFromGitHub {
+    owner = "tektoncd";
+    repo = "cli";
+    rev = "v${version}";
+    sha256 = "0dj4c703cdl28x9fi42ydbw8ml24bhfrcyafvpdgj7pvajbr3nlh";
+  };
+
+  meta = with stdenv.lib; {
+    homepage    = https://github.com/tektoncd/cli;
+    description = "A CLI for interacting with Tekton!";
+    license     = licenses.asl20;
+    maintainers = with maintainers; [ vdemeester ];
+  };
+}
