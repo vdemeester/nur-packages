70136dd8c939af148d40a4f862073108cd4acc8e
Vincent Demeester <vincent@sbr.pm>
2019-07-26 12:29:24 +0200

    Replace . by _ in target names
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- hack/pkgs-build-cachix.sh
+++ hack/pkgs-build-cachix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-channel=$(echo $CHANNEL | grep / | cut -d/ -f5-)
+channel=$(echo $CHANNEL | grep / | cut -d/ -f5- | sed -r 's/\./_/g')
 nix-build --no-out-link . -A allTargets.$channel | cachix push shortbrain
 
