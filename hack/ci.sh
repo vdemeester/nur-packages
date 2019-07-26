007bfe933dc20d9184229a770d3ae657958ec82c
Vincent Demeester <vincent@sbr.pm>
2019-07-26 12:43:03 +0200

    Debug travis ci
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- hack/ci.sh
+++ hack/ci.sh
@@ -6,8 +6,8 @@ nix-channel --add ${CHANNEL} nixpkgs
 nix-channel --list
 nix-channel --update
 
-$(dirname $0)/pkgs-build-cachix.sh
-
 if test "${CHANNEL}" = "https://nixos.org/channels/nixpkgs-unstable"; then
     curl -XPOST "https://nur-update.herokuapp.com/update?repo=vdemeester"
 fi
+
+$(dirname $0)/pkgs-build-cachix.sh
--- hack/pkgs-build-cachix.sh
+++ hack/pkgs-build-cachix.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -x
 
 channel=$(echo $CHANNEL | grep / | cut -d/ -f5- | sed -r 's/\./_/g')
 nix-build --no-out-link . -A allTargets.$channel | cachix push shortbrain
